744ebdd0f35b30e1042c6a3589559e77bf04b2f4
Camilo Catasus <camilomcatasus@gmail.com>
2023-07-13 10:47:50 -0400

    Fixed harpoon stuff

--- lua/cambucha/packer.lua
+++ lua/cambucha/packer.lua
@@ -6,7 +6,10 @@ return require('packer').startup(function(use)
 
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		requires = {{'nvim-lua/plenary.nvim'}}
+		requires = {
+			{'nvim-lua/plenary.nvim'},
+			{'BurntSushi/ripgrep'}
+	}
 	
 		
 	}
