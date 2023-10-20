bb2ad3966ebadd28f1baad7b63df710fc304ffb9
Camilo Catasus <camilomcatasus@gmail.com>
2023-10-20 09:26:39 -0400

    Node Tree changes

--- init.lua
+++ init.lua
@@ -7,7 +7,17 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 require('nvim-web-devicons').setup()
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    log = {
+        enable = true,
+        types = {
+            watcher = true
+        }
+    },
+    filesystem_watchers = {
+        ignore_dirs = {".target"}
+    }
+})
 
 
 require("cambucha")
